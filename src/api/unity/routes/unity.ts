600abd8339ae823251ba5a6ea0fcbff4fccd6485
Brunic Feyou <feyoubrunic2@gmail.com>
2025-01-19 00:42:21 +0100

    installation de dépendences

---
+++ src/api/unity/routes/unity.ts
@@ -0,0 +1,7 @@
+/**
+ * unity router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::unity.unity');
---
+++ src/api/unity/services/unity.ts
@@ -0,0 +1,7 @@
+/**
+ * unity service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::unity.unity');
